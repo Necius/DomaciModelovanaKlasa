a191329705b82bf75851cb5a17c7b74a0e9c19a7
Necius <60194069+Necius@users.noreply.github.com>
2020-02-12 09:29:53 +0100

    Add files via upload

---
+++ Selenium/src/seleniumtest/GoogleTest.java
@@ -0,0 +1,21 @@
+package seleniumtest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class GoogleTest {
+
+	@Test
+	void testLogoPresence() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\neman\\Downloads\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com/");
+		boolean logoPresent = driver.findElement(By.id("hplogo")).isDisplayed();
+		driver.close();
+		assertTrue(logoPresent);
+	}
+
+}
